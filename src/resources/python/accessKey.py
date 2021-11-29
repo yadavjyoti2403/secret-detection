1b971456282ea45bbc3ece5fd9b0d8c32ae316b2
Jyoti Yadav <jyoti.yadav@thepsi.com>
2021-11-29 11:07:37 +0530

    Added Sample secrets.

---
+++ src/resources/python/accessKey.py
@@ -0,0 +1,6 @@
+username='admin'
+password='password@1234'
+db_pass_prod='LMT03HYVIFJH56KJA08HGKGJ^&JHGKJKHJL'
+db_pass_UAT='p@$$w0rd@1234'
+accessKey = 'AKIAI6B5TG3Q6H32ULTA'
+accessKey = 'AKIAI6HUYTPLH32ULTAB'
